f40c897092575a042febdf60dfc9985ee5648ab6
hjiang13 <hjiang13@kent.edu>
2024-03-21 11:56:25 -0400

    train_resilience.sh modified

--- code/train_resilience.sh
+++ code/train_resilience.sh
@@ -3,13 +3,13 @@ python run.py \
     --tokenizer_name=neulab/codebert-cpp \
     --model_name_or_path=neulab/codebert-cpp \
     --do_train \
-    --train_data_file=../dataset/test_resilience.json \
-    --eval_data_file=../dataset/valid.jsonl \
-    --test_data_file=../dataset/test.jsonl \
+    --train_data_file=../dataset/train_resilience.json \
+    --eval_data_file=../dataset/valid_resilience.jsonl \
+    --test_data_file=../dataset/test_resilience.jsonl \
     --num_train_epochs 20 \
     --block_size 256 \
-    --train_batch_size 8 \
-    --eval_batch_size 16 \
+    --train_batch_size 4 \
+    --eval_batch_size 4 \
     --learning_rate 2e-5 \
     --max_grad_norm 1.0 \
     --seed 123456  2>&1 | tee train.log
